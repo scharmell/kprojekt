0930ef7f66fbafc3a62e7b9ef14a01179980b9b1
Scharmel <kamenevdel@yandex.ru>
2025-04-14 16:41:53 +0300

    fixes

---
+++ home_works_kurs2/src/masks.py
@@ -0,0 +1,26 @@
+from typing import Union
+
+card_number: str = input("Введите номер карты:")
+account_number: str = input("Введите номер счета:")
+"""Получаем данные от пользователя"""
+
+
+def get_mask_card_number(card_number: Union[str]) -> Union[str]:
+    """Возвращаем замаскированный номер карты"""
+    len_number = 16
+    if len(card_number) == len_number:
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    else:
+        return "Вы ввели некорректные данные"
+
+def get_mask_account(account_number: Union[str]) -> Union[str]:
+    """Возвращаем замаскированный номер счета"""
+    len_acc_num = 20
+    if len(account_number) == len_acc_num:
+        return f"**{account_number[-4:]}"
+    else:
+        return "Вы ввели некорректные данные"
+
+
+print(get_mask_card_number(card_number))
+print(get_mask_account(account_number))
