5c69238f1ec7cd1c743d86e5edfe367ffa98e164
Scharmel <kamenevdel@yandex.ru>
2025-04-18 13:52:46 +0300

    fixes homework 9.2

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,5 @@
-from typing import Union
 
-card_number: str = input("Введите номер карты:")
-account_number: str = input("Введите номер счета:")
-"""Получаем данные от пользователя"""
-
-
-def get_mask_card_number(card_number: Union[str]) -> Union[str]:
+def get_mask_card_number(card_number: str) -> str:
     """Возвращаем замаскированный номер карты"""
     len_number = 16
     if len(card_number) == len_number:
@@ -14,7 +8,7 @@ def get_mask_card_number(card_number: Union[str]) -> Union[str]:
         return "Вы ввели некорректные данные"
 
 
-def get_mask_account(account_number: Union[str]) -> Union[str]:
+def get_mask_account(account_number: str) -> str:
     """Возвращаем замаскированный номер счета"""
     len_acc_num = 20
     if len(account_number) == len_acc_num:
@@ -23,5 +17,3 @@ def get_mask_account(account_number: Union[str]) -> Union[str]:
         return "Вы ввели некорректные данные"
 
 
-print(get_mask_card_number(card_number))
-print(get_mask_account(account_number))
--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+from src.masks import get_mask_account, get_mask_card_number
+
 def get_date(date: str) -> str:
     """ Функция работы с данными """
     year = date[:4]
@@ -5,4 +7,14 @@ def get_date(date: str) -> str:
     day = date[8:10]
     return f"{day}.{month}.{year}"
 
-
+def mask_account_card(number: str) -> str:
+    """ Функция маскировки """
+    list_number = number.split()
+    number_card = list_number.pop()
+    name_card = " ".join(list_number)
+    if name_card == "Счет":
+        return f"Счет {get_mask_account(number_card)}"
+    else:
+        return f"{name_card} {get_mask_card_number(number_card)}"
+print(mask_account_card("Visa Platinum 8990922113665229"))
+print(mask_account_card("Счет 73654108430135874305"))
