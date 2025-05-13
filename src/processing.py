5692258f707ca4422beca4c21b36c3a4de864ccf
Scharmel <kamenevdel@yandex.ru>
2025-05-13 19:33:59 +0300

    correction of errors homework_10_1

--- src/processing.py
+++ src/processing.py
@@ -10,15 +10,15 @@ def filter_by_state(transactions: List[Dict], state: str = "EXECUTED") -> List[D
     return results
 
 
-def sort_by_date(transactions: List[Dict], reverse: bool = True) -> List[Dict]:
+def sort_by_date(transactions: List[Dict], ascending: bool = True) -> List[Dict]:
     """Сортирует заданный список словарей по дате"""
-    return sorted(transactions, reverse=reverse, key=lambda t: t["date"])
+    return sorted(transactions, reverse=ascending, key=lambda t: t["date"])
 
 
 print(
     filter_by_state(
         [
-            {"id": 41428829, "state": "Canceled", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         ]
     )
@@ -26,7 +26,7 @@ print(
 print(
     sort_by_date(
         [
-            {"id": 41428829, "state": "Canceled", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         ]
     )
